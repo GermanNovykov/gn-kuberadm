26ac9008fbde5c9e54222e7f4636dcd583dbbc37
German Novykov <113905743+GermanNovykov@users.noreply.github.com>
2023-12-10 12:30:20 +0100

    new commit w fix

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"c897309e61514c44cc83cba9df48926a525dcdfc30fe160c758b23098823e15d","vagrant_version":"2.3.7"}
---
+++ configs/join.sh
@@ -0,0 +1,1 @@
+kubeadm join 10.0.0.10:6443 --token 04rib5.6213vomb3wxa49qi --discovery-token-ca-cert-hash sha256:7762b31ab32d014867014b199c1d08b9c211ae1e67ff54c660dcc3521be5210c 
